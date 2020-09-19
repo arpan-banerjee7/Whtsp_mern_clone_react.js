3e11ea12b7a33f8c15692b273a7bfac8116f8954
Arpan619Banerjee <arpan.nbanerjee7@gmail.com>
2020-09-19 12:57:35 +0530

    barebone set up with chatr and sidebar components

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCYoJU_K-141Ht-QcBRVHZ-YmBAvFVzN6c",
+  authDomain: "whatsapp-mern-bc8df.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-bc8df.firebaseio.com",
+  projectId: "whatsapp-mern-bc8df",
+  storageBucket: "whatsapp-mern-bc8df.appspot.com",
+  messagingSenderId: "831890637947",
+  appId: "1:831890637947:web:0f4b2d1ea183ec34414ede",
+};
